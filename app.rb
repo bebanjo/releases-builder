d0b9aa63945514e1d74a7f92f601bf35734a7a2a
Marcelino Llano <hello@marcelinollano.com>
2020-12-18 14:51:54 +0100

    Sets headers correctly

--- app.rb
+++ app.rb
@@ -6,14 +6,16 @@ class App < Sinatra::Base
 
   post('/ping/?') do
     Thread.new{ system('./bin/build') }
+    content_type 'text/plain'
     '202 Accepted'
   end
 
   get('/?') do
+    content_type 'text/plain'
     '200 OK'
   end
 
   not_found do
-    halt(404, '404 Not Found')
+    halt 404, {'Content-Type' => 'text/plain'}, '404 Not Found'
   end
 end
